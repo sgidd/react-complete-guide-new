3107a21d3c61b6d58704bd692efa6d47c9ce0a6a
sunil <sunilgidd051@gmail.com>
2020-04-30 13:51:35 +0530

    Outputting Burger Ingredients Dynamically - usage of Object.keys and Arrya()

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,25 @@ import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
 
+    // constructor(props){
+    //     super(props);
+    //     this.state = {...};
+
+    // }
+
+    state = {
+        ingredients : {
+            salad :0,
+            bacon:1,
+            cheese:0,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                
                 <div>Build Controls</div>
             </Aux>
