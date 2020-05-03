f6683e8d5f9356d10e3f79a80d79cd1569421a87
sunil <sunilgidd051@gmail.com>
2020-05-03 13:25:17 +0530

    Complete sidedrawer implementation with hamburger icon and backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxillary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
 
-export default layout;
+class Layout extends Component {
+
+    state={
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () =>{
+        // const showSideDrawer = this.state.showSideDrawer;
+        this.setState({showSideDrawer : false});
+    }
+
+    sideDrawerToggleHandler = () =>{
+        // this.setState({showSideDrawer: !this.state.showSideDrawer});// this approch has flaw
+
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,19 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <Logo />
-        <nav>
+        {/* <div onClick={props.toggleSideDrawer} >Menu</div> */}
+
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+
+        {/* <Logo  height="80%"/> */}
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
