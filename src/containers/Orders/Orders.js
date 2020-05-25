9570ed34eaa7afaec272be434882fc95c35271b7
sunil <sunilgidd051@gmail.com>
2020-05-26 01:34:23 +0530

    added withErrorHandler to Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component{
     state={
@@ -37,4 +38,4 @@ class Orders extends Component{
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
