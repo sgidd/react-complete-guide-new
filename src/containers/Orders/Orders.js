efb45791b80316378f5b26f1b7332c2766f5bdbe
sunil <sunilgidd051@gmail.com>
2020-05-26 02:07:50 +0530

    Outputting the Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -31,8 +31,14 @@ class Orders extends Component{
         console.log(this.state.orders);
         return(
             <div>
-                <Order />
-                <Order />
+                {
+                    this.state.orders.map(order => (
+                        <Order 
+                            key= {order.id} 
+                            ingredients={order.ingredients }
+                            price={order.price}/>
+                    ))
+                }
             </div>
         )
     }
