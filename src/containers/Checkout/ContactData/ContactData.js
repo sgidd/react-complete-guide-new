65090d22790dccf703826b591370f7935c520277
sunil <sunilgidd051@gmail.com>
2020-05-26 16:29:12 +0530

    Adding a Dropdown Component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -53,7 +53,7 @@ class ContactData extends Component {
                     elementType: 'select',
                     elementConfig: {
                         options : [
-                            {value: 'fastest', displayValue:'fastest'},
+                            {value: 'fastest', displayValue:'Fastest'},
                             {value: 'cheapest', displayValue:'Cheapest'}
                         ]
                     },
@@ -97,7 +97,6 @@ class ContactData extends Component {
 
         let form = (
             <form>
-            
                 {
                     formElementsArray.map(formElement => (
                         <Input 
