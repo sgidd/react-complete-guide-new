e3776d8baac0eccf38d65a797158e5712c8ac59f
sunil <sunilgidd051@gmail.com>
2020-06-04 23:08:47 +0530

    Combining Reducers

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -13,7 +13,6 @@ class Checkout extends Component{
         this.props.history.replace('/checkout/contact-data');
     }
     render() {
-
         let summary = <Redirect to="/" />
         if(this.props.ings){
             summary =   (
@@ -33,7 +32,7 @@ class Checkout extends Component{
 
 const mapStateToProps = state => {
     return{
-        ings : state.ingredients
+        ings : state.burgerBuilder.ingredients
     }
 }
 export default connect(mapStateToProps)(Checkout);
