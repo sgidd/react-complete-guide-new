5b279543fc21db3f0fbee6175c28ad23c914d737
sunil <sunilgidd051@gmail.com>
2020-06-13 12:54:59 +0530

    using updateObject in entire app and adding email validation in contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import {connect} from 'react-redux';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import {updateObject} from '../../../shared/utility';
 
 class ContactData extends Component {
     state={
@@ -74,7 +75,8 @@ class ContactData extends Component {
                     },
                     value:'',
                     validation: {
-                        required: true 
+                        required: true,
+                        isEmail: true
                     },
                     valid: false,
                     touched: false
@@ -99,23 +101,22 @@ class ContactData extends Component {
 
     checkValidity(value, rules){
         let isValid = true;
-
         if(!rules){
             return true;
-        }
-        
+        }     
         if(rules.required){
             isValid =  value.trim() !== '' && isValid;
         }
-
         if(rules.minLength){
             isValid = value.length >= rules.minLength && isValid;
         }
-
         if(rules.maxLength){
             isValid = value.length <= rules.maxLength && isValid;
         }
-
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
         return isValid;
     }
 
@@ -140,20 +141,14 @@ class ContactData extends Component {
     }
 
     inputChangedHandler = (event, inputIdentifier) => {
-        //console.log(event.target.value);
-        const updatedOrderForm ={
-            ...this.state.orderForm
-        };
-        const updatedFormElement = {
-            ...updatedOrderForm[inputIdentifier]
-        };
-
-        updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value , updatedFormElement.validation)
-        updatedFormElement.touched = true;
-        updatedOrderForm[inputIdentifier] = updatedFormElement;
-        console.log(updatedFormElement);
-
+        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier],{
+            value: event.target.value,
+            valid: this.checkValidity(event.target.value , this.state.orderForm[inputIdentifier].validation),
+            touched: true
+        });  
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier]: updatedFormElement
+        })
         let formIsValid = true;
         for(let inputIdentifier in updatedOrderForm){
             formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
