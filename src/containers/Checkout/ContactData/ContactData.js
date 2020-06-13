c36aedfbaa6499c350a293e08242c1c8d5b31d78
sunil <sunilgidd051@gmail.com>
2020-06-13 13:06:09 +0530

    Sharing the Validation Method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,7 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import {connect} from 'react-redux';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import {updateObject} from '../../../shared/utility';
+import {updateObject, checkValidity} from '../../../shared/utility';
 
 class ContactData extends Component {
     state={
@@ -99,27 +99,6 @@ class ContactData extends Component {
         // loading:false
     }
 
-    checkValidity(value, rules){
-        let isValid = true;
-        if(!rules){
-            return true;
-        }     
-        if(rules.required){
-            isValid =  value.trim() !== '' && isValid;
-        }
-        if(rules.minLength){
-            isValid = value.length >= rules.minLength && isValid;
-        }
-        if(rules.maxLength){
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-        return isValid;
-    }
-
     orderHandler = (event) => {
         event.preventDefault();
         console.log(this.props.ingredients);
@@ -143,7 +122,7 @@ class ContactData extends Component {
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier],{
             value: event.target.value,
-            valid: this.checkValidity(event.target.value , this.state.orderForm[inputIdentifier].validation),
+            valid: checkValidity(event.target.value , this.state.orderForm[inputIdentifier].validation),
             touched: true
         });  
         const updatedOrderForm = updateObject(this.state.orderForm, {
